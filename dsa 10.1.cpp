5c3796f486ffd6ec22c704e9d225f1a8a26479ef
Anil-1031 <118690285+Anil-1031@users.noreply.github.com>
2022-11-21 02:30:39 +0530

    Add files via upload

---
+++ dsa 10.1.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node
+{
+int data;
+struct node *left;
+struct node *right;
+};
+
+struct node *tree;
+
+void create_tree(struct node *);
+struct node *insertElement(struct node *, int);
+int countLeaf(struct node *);
+
+int main()
+{
+int option, val;
+struct node *ptr;
+create_tree(tree);
+do
+{
+printf("\n 1. Insert Element");
+printf("\n 2. Count Leaf Nodes");
+printf("\n 3. Exit");
+printf("\n\n Enter your option : ");
+scanf("%d", &option);
+switch(option)
+ {
+case 1:
+printf("\n Enter the value of the new node : ");
+scanf("%d", &val);
+tree= insertElement(tree, val);
+break;
+case 2:
+printf("\n The number of leaf nodes are : %d\n",countLeaf(tree) );
+break;
+}
+}while(option!=3);
+return 0;
+}
+
+void create_tree(struct node *tree)
+{
+tree = NULL;
+}
+
+struct node *insertElement(struct node *tree, int val)
+{
+struct node *ptr, *nodeptr, *parentptr;
+ptr = (struct node*)malloc(sizeof(struct node));
+ptr->data = val;
+ptr->left = NULL;
+ptr->right = NULL;
+if(tree==NULL)
+{
+tree=ptr;
+tree->left=NULL;
+tree->right=NULL;
+}
+else
+{
+parentptr=NULL;
+nodeptr=tree;
+while(nodeptr!=NULL)
+ {
+parentptr=nodeptr;
+if(val<nodeptr->data)
+nodeptr=nodeptr->left;
+else
+nodeptr = nodeptr->right;
+ }
+ 
+if(val<parentptr->data)
+parentptr->left = ptr;
+else
+parentptr->right = ptr;
+}
+return tree;
+}
+
+int countLeaf(struct  node* tree){
+	if(tree==NULL)
+	return 0;
+	if(tree->left==NULL&&tree->right==NULL)
+	return 1;
+	else
+	return countLeaf(tree->left)+countLeaf(tree->right);
+}
